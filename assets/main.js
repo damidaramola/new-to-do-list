3a94484328cec3ed29250c4b7e1bcea2ab487b3a
damidaramola <110638513+damidaramola@users.noreply.github.com>
2023-05-08 07:49:21 +0100

    create elements to add new item in todo list

--- assets/main.js
+++ assets/main.js
@@ -13,13 +13,42 @@ window.addEventListener('load', ()=>{
     });
 
     form.addEventListener('submit',e => {
-    e.preventDefault();
-    })
-    
+    e.preventDefault(); 
     const todoList ={
-      content: e.target.elements.content.value,
-      done:false,
+      content:e.target.elements.content.value,
+      done:false
     }
 
-    
+    todos.push(todoList);
+    localStorage.setItem('todos',JSON.stringify(todos));
+    e.target.reset();
+
+    ShowTodos();
 });
+  
+ShowTodos();  
+
+});
+
+function ShowTodos(){
+    const newTodos = documenr.querySelector('#list');
+    newTodos.innerHTML ='';
+
+    todos.forEach(todo => {
+        const todoItem = document.createElement('div')
+        todoItem.classList.add('todo-item')
+
+        const label = document.createElement('label');
+		const input = document.createElement('input');
+		const span = document.createElement('span');
+		const content = document.createElement('div');
+		const actions = document.createElement('div');
+		const editButton = document.createElement('button');
+		const deleteButton = document.createElement('button');
+
+        
+		input.type = 'checkbox';
+		input.checked = todo.done;
+		span.classList.add('radio-button');
+    })
+}
